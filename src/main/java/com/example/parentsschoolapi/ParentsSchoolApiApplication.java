25d8e21b9c70d9b335ba78c18bd538286e06c52e
leyvidson <leyvidson@gmail.com>
2023-09-26 16:38:09 -0300

    primer commit

---
+++ src/main/java/com/example/parentsschoolapi/ParentsSchoolApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.parentsschoolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentsSchoolApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParentsSchoolApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/parentsschoolapi/ParentsSchoolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parentsschoolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentsSchoolApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
